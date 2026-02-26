9c67c5b0b708d1d8c11390be827d82e843a56921
inimai09 <inimaisudar@gmail.com>
2026-02-26 09:10:01 +0530

    UC2:palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,13 @@
 public static void main(String[] args){
-    System.out.println("Welcome to palindrome Checker Management System");
-    System.out.println("Version : 1.0");
-    System.out.println("System initialized successfully");
+    String input = "madam";
+    boolean flag = true;
+
+    for(int i = 0; i < input.length()/2; i++){
+        if(input.charAt(i) != input.charAt(input.length() - 1 -i)){
+            flag = false;
+            break;
+        }
+    }
+    System.out.println("Input text:" + input);
+    System.out.println("Is it a Palindrome:" + flag);
 }
